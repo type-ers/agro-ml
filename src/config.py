60c1d828b47ae17b4449408badf1d6fca19bdd67
ItsSitanshu <sitanshu15shrestha@gmail.com>
2024-07-05 12:32:52 +0545

    requirements.txt

--- src/config.py
+++ src/config.py
@@ -1,21 +1,10 @@
-import requests
 import pandas as pd
 import numpy as np
-from io import BytesIO
 
 from flask import *
-from flask_bcrypt import *
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import LargeBinary
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, PasswordField, SubmitField, EmailField
-
-from wtforms.validators import InputRequired, Length, ValidationError, Email 
 
 from os.path import abspath, join
 from os import listdir
-from base64 import b64encode
 from joblib import load
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
